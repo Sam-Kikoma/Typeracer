fca03bff32538ea6dc3fddf4263e2d8304819908
Sam Kikoma <kikomasamson@gmail.com>
2025-10-15 19:39:12 +0200

    feat: initialize auth service with express

---
+++ services/auth/src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+	res.send("<p>Auth service is live</p>");
+});
+export default app;
