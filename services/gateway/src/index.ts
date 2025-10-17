080891c389ee1954ec1bec187ccc499472508857
Sam Kikoma <kikomasamson@gmail.com>
2025-10-17 15:06:15 +0200

    chore: set up express middleware and proxy for services

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -1,13 +1,29 @@
 import express from "express";
+import cors from "cors";
 import { PORT } from "./utils/config.ts";
 import type { Response } from "express";
+import morgan from "morgan";
+import { services } from "./routes.ts";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("combined"));
+app.disable("x-powered-by");
 
 app.get("/", (_, res: Response) => {
 	res.send("Gateway is live");
 });
 
+services.forEach(({ route, target }) => {
+	const proxyOptions = {
+		target,
+		changeOrigin: true,
+	};
+	app.use(route, createProxyMiddleware(proxyOptions));
+});
+
 app.listen(PORT, () => {
-	console.log(`Gateay is running on ${PORT}`);
+	console.log(`Gateway is running on ${PORT}`);
 });
