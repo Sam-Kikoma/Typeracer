d04d5b0f0de598c860da6577c1e4600ccbf039af
Sam Kikoma <kikomasamson@gmail.com>
2025-10-17 11:18:01 +0200

    feat: add user routes

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import type { Request, Response } from "express";
+import userRouter from "./routes/user.ts";
 
 const app = express();
 
 app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
 	res.send("<p>Auth service is live</p>");
 });
+
+app.use("/api/users", userRouter);
+
 export default app;
