50a063447f5817462669405d9242c4a035c03af9
Sam Kikoma <kikomasamson@gmail.com>
2025-10-17 15:06:04 +0200

    chore: add initial routes for authentication

---
+++ services/gateway/src/routes.ts
@@ -0,0 +1,10 @@
+export const services = [
+	{
+		route: "/api/auth/login",
+		target: "http://localhost:3001/api/auth/login",
+	},
+	{
+		route: "/api/auth/signup",
+		target: "http://localhost:3001/api/auth/signup",
+	},
+];
