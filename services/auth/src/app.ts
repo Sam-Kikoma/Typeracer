6da4effa34d800647f101c9aecb273c25892035a
Sam Kikoma <kikomasamson@gmail.com>
2025-10-17 15:06:50 +0200

    chore: update auth routes and add CORS

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import type { Request, Response } from "express";
 import userRouter from "./routes/user.ts";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api/auth", (req: Request, res: Response) => {
 	res.send("<p>Auth service is live</p>");
 });
 
-app.use("/api/users", userRouter);
+app.use("/api/auth", userRouter);
 
 export default app;
