56a5ab49e2a2f8969dc488c60c766a44906b1de8
Sam Kikoma <kikomasamson@gmail.com>
2025-11-13 20:04:38 +0100

    feat: add routing for authentication and game pages

--- clients/typeracerWeb/src/App.tsx
+++ clients/typeracerWeb/src/App.tsx
@@ -1,5 +1,8 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import AuthForm from "./components/AuthForm";
+import { Toaster } from "./components/retroui/Sonner";
+import Game from "./pages/Game";
 
 const App = () => {
 	return (
@@ -7,6 +10,11 @@ const App = () => {
 			<main className="p-4">
 				<Navbar />
 			</main>
+			<Routes>
+				<Route path="/auth" element={<AuthForm />} />
+				<Route path="/game" element={<Game />} />
+			</Routes>
+			<Toaster />
 		</Router>
 	);
 };
