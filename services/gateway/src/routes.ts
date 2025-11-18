0522d0396f9d7654bd9acf31fdc569f9c6043d4e
Sam Kikoma <kikomasamson@gmail.com>
2025-11-18 17:32:26 +0100

    feat: update routes to include protected status for game room

--- services/gateway/src/routes.ts
+++ services/gateway/src/routes.ts
@@ -2,9 +2,16 @@ export const services = [
 	{
 		route: "/api/auth/login",
 		target: "http://localhost:3001/api/auth/login",
+		protected: false,
 	},
 	{
 		route: "/api/auth/signup",
 		target: "http://localhost:3001/api/auth/signup",
+		protected: false,
+	},
+	{
+		route: "/api/gameRoom",
+		target: "http://localhost:3002/api/game",
+		protected: true,
 	},
 ];
